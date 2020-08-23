0e820b22bda628f80eac9ddcda159042a5e9cffd
gradybknight <grady.knight@gmail.com>
2020-08-22 21:55:50 -0400

    make run on multiple platforms

--- interactive_prompt/prompt.c
+++ interactive_prompt/prompt.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+/* inject functions if compiling on windows */
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+char* readline(char* prompt) {
+	fputs(prompt, stdout);
+	fgetts(buffer, 2048, stdin);
+	char* cpy = malloc(strlen(buffer)+1);
+	strcpy(cpy, buffer);
+	cpy[strlen(cpy-1)] = '\0';
+	return cpy;
+}
+
+void add_history(char* unused) {}
+
+/* inject functions if compiling on windows */
+#else
 #include <editline/readline.h>
+#endif
 
 int main(int argc, char** argv) {
 	puts("Lispy Version 0.0.0.0.1");
